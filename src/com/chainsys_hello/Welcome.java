0c75e699dd6dfbdc74664297c2958767322f19a7
Pavithra G <pavi2107@global.chainsys.com>
2019-02-14 12:58:04 +0530

    From eclipse

---
+++ src/com/chainsys_hello/Welcome.java
@@ -0,0 +1,11 @@
+package com.chainsys_hello;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vanakam");
+
+	}
+
+}
